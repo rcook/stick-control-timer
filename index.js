3bde83768f61fcaa12d8cc67ab9d4f121f18749e
Richard Cook <rcook@rcook.org>
2026-01-15 16:01:09 -0800

    Add presets

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const DEFAULT_COUNTDOWN_DURATION = 120;
 const DEFAULT_ALERT_DURATION = 10;
 const DEFAULT_PAUSE_DURATION = 5;
 
+const PRESETS = document.getElementById("presets");
 const START_BUTTON = document.getElementById("start");
 const STOP_BUTTON = document.getElementById("stop");
 const RESET_BUTTON = document.getElementById("reset");
@@ -11,6 +12,37 @@ const COUNTDOWN = document.getElementById("countdown");
 const COUNTDOWN_DURATION_INPUT = document.getElementById("countdown-duration");
 const ALERT_DURATION_INPUT = document.getElementById("alert-duration");
 const PAUSE_DURATION_INPUT = document.getElementById("pause-duration");
+
+function h(s) {
+  return new Option(s).innerHTML;
+}
+
+function attr(s) {
+  return new Option(s).innerHTML.replace("\"", "&quot;");
+}
+
+function showPresets(presets) {
+  const ul = document.createElement("ul");
+  PRESETS.appendChild(ul)
+  for (const preset of presets) {
+    const li = document.createElement("li");
+    ul.appendChild(li);
+
+    const button = document.createElement("button");
+    li.appendChild(button);
+
+    button.countdownDuration = preset.countdownDuration;
+    button.alertDuration = preset.alertDuration;
+    button.pauseDuration = preset.pauseDuration;
+    const s = `${preset.name} (${preset.countdownDuration}/${preset.alertDuration}/${preset.pauseDuration})`;
+    button.innerText = s;
+    button.addEventListener("click", e => {
+      COUNTDOWN_DURATION_INPUT.value = e.target.countdownDuration;
+      ALERT_DURATION_INPUT.value = e.target.alertDuration;
+      PAUSE_DURATION_INPUT.value = e.target.pauseDuration;
+    });
+  }
+}
 
 const TimerState = Object.freeze({
   IDLE: 0,
@@ -30,14 +62,17 @@ class Timer {
     this.startTime = null;
   }
 
-  start() {
+  start(countdownDuration, alertDuration, pauseDuration) {
     if (this.state != TimerState.IDLE) { return; }
 
+    this.countdownDuration = countdownDuration;
+    this.alertDuration = alertDuration;
+    this.pauseDuration = pauseDuration;
     this.state = TimerState.RUNNING;
     this.setCountdownClass();
     this.startTime = Date.now();
     let self = this;
-    this.intervalId = setInterval(function () { self.step() }, 10);
+    this.intervalId = setInterval(() => { self.step() }, 10);
   }
 
   stop() {
@@ -114,19 +149,29 @@ class Timer {
 
 const TIMER = new Timer(DEFAULT_COUNTDOWN_DURATION, DEFAULT_ALERT_DURATION, DEFAULT_PAUSE_DURATION, "next");
 
-START_BUTTON.addEventListener("click", function () {
-  TIMER.countdownDuration = parseInt(COUNTDOWN_DURATION_INPUT.value);
-  TIMER.alertDuration = parseInt(ALERT_DURATION_INPUT.value);
-  TIMER.pauseDuration = parseInt(PAUSE_DURATION_INPUT.value);
-  TIMER.start();
+START_BUTTON.addEventListener("click", () => {
+  const countdownDuration = parseInt(COUNTDOWN_DURATION_INPUT.value);
+  const alertDuration = parseInt(ALERT_DURATION_INPUT.value);
+  const pauseDuration = parseInt(PAUSE_DURATION_INPUT.value);
+  TIMER.start(countdownDuration, alertDuration, pauseDuration);
 });
 
-STOP_BUTTON.addEventListener("click", function () {
+STOP_BUTTON.addEventListener("click", () => {
   TIMER.stop();
 });
 
-window.onload = function () {
+window.onload = () => {
   COUNTDOWN_DURATION_INPUT.value = DEFAULT_COUNTDOWN_DURATION;
   ALERT_DURATION_INPUT.value = DEFAULT_ALERT_DURATION;
   PAUSE_DURATION_INPUT.value = DEFAULT_PAUSE_DURATION;
 };
+
+
+fetch("data.json")
+  .then(response => response.json())
+  .then(data => {
+    if (Object.hasOwn(data, "presets")) {
+      showPresets(data.presets);
+    }
+  })
+  .catch(e => alert(`Could not parse JSON from server: ${e}\n\nPlease ask Richard to fix this!`));
